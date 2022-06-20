3ee2e2e99c083c633d33a5ee6da2a6ee8b5bed93
Afnan <hmafnan3@gmail.com>
2022-06-21 00:18:32 +0500

    Update server.js

--- hello-world-nodejs-docker/server.js
+++ hello-world-nodejs-docker/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello afnan\n welcome to nodejs using docker & jenkins pipline');
 });
 
 app.listen(PORT, HOST);
